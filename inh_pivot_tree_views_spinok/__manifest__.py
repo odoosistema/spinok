09fdef105a892010be129afa7d6ec28287a17b48
odoosistema <68307968+odoosistema@users.noreply.github.com>
2022-05-12 12:54:35 -0400

    Update __manifest__.py

--- inh_pivot_tree_views_spinok/__manifest__.py
+++ inh_pivot_tree_views_spinok/__manifest__.py
@@ -2,11 +2,11 @@
 # Copyright (C) Softhealer Technologies.
 {
     'name': 'Inherit views pivot tree spinok',
-    'version': '14',
+    'version': '15',
     'license': 'LGPL-3',
     'summary': 'Modulo para añadir campo codigo de barra en el modelo stock en las vistas pivot y tree',
     'category': '',
-    'author': 'LAngel Cartaya ',
+    'author': 'LAngel Cartaya',
     'depends': ['base', 'stock', 'sale', 'product_multiple_barcodes','inh_product_spinok'],
     'data': [
         'views/stock_quant_inh_view.xml','views/purchase_order_inh_view.xml',
