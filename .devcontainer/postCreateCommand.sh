1117408313e1236cb0986b609505260d57138740
DHerrmannP <dherrmann@addval.cl>
2025-06-09 14:11:59 +0000

    DevelopmentEnvironment - Remove dependency on AddvalModules

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-requirements_path="/mnt/extra-addons/Addval-Connect/odoo-addval-modules/requirements.txt"
+requirements_path="/mnt/extra-addons/requirements.txt"
 if [ -e $requirements_path ]; then
     echo "Installing requirements..."
     pip install -r $requirements_path --break-system-packages
