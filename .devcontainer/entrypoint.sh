0440e1c076c1e7d348aa7206b75e75cc83984ce7
DHerrmannP <dherrmann@addval.cl>
2025-06-27 15:20:17 -0400

    devcontainer - updated

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -13,6 +13,13 @@ fi
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
+PSQL_ARGS=()
+function check_psql_config() {
+    param="$1"
+    value="$2"
+    PSQL_ARGS+=("--${param}")
+    PSQL_ARGS+=("${value}")
+}
 DB_ARGS=()
 function check_config() {
     param="$1"
@@ -28,7 +35,16 @@ check_config "db_port" "$PORT"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
 
+check_psql_config "host" "$HOST"
+check_psql_config "port" "$PORT"
+check_psql_config "username" "$USER"
+
 case "$1" in
+    pdb | dbg | debug | debugpy)
+        shift
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+        exec python3 -m debugpy --listen 5678 "$ODOO_PATH/odoo-bin" "$@" "${DB_ARGS[@]}"
+        ;;
     -- | odoo)
         shift
         if [[ "$1" == "scaffold" ]] ; then
@@ -41,6 +57,10 @@ case "$1" in
     -*)
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
         exec odoo-bin "$@" "${DB_ARGS[@]}"
+        ;;
+    psql)
+        shift
+        exec env PGPASSWORD=${PASSWORD} psql "$@" "${PSQL_ARGS[@]}"
         ;;
     *)
         exec "$@"
